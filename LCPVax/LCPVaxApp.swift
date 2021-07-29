6367bcab671fbf4e09f91bb963222a5387aa74e9
Theodore Fausak <tmfausak@gmail.com>
2021-07-29 15:24:56 -0500

    Initial Commit

---
+++ LCPVax/LCPVaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCPVaxApp.swift
+//  LCPVax
+//
+//  Created by Theodore Fausak on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LCPVaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
